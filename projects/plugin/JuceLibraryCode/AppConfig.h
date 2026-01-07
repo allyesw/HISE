06629a42d391578c78232b5ebfd5305aa78380a0
allyrhodesdev <allyrhodesdev@gmail.com>
2026-01-07 13:30:39 -0800

    pre-update to fix multichannel plugin crash

--- projects/plugin/JuceLibraryCode/AppConfig.h
+++ projects/plugin/JuceLibraryCode/AppConfig.h
@@ -27,12 +27,12 @@
 /*
   ==============================================================================
 
-   In accordance with the terms of the JUCE 7 End-Use License Agreement, the
+   In accordance with the terms of the JUCE 6 End-Use License Agreement, the
    JUCE Code in SECTION A cannot be removed, changed or otherwise rendered
    ineffective unless you have a JUCE Indie or Pro license, or are using JUCE
    under the GPL v3 license.
 
-   End User License Agreement: www.juce.com/juce-7-licence
+   End User License Agreement: www.juce.com/juce-6-licence
 
   ==============================================================================
 */
@@ -47,7 +47,7 @@
 
 #define JUCE_USE_DARK_SPLASH_SCREEN 1
 
-#define JUCE_PROJUCER_VERSION 0x7000c
+#define JUCE_PROJUCER_VERSION 0x60104
 
 //==============================================================================
 #define JUCE_MODULE_AVAILABLE_hi_backend                    1
@@ -319,7 +319,7 @@
 // hi_faust flags:
 
 #ifndef    HISE_INCLUDE_FAUST
- #define   HISE_INCLUDE_FAUST 1
+ #define   HISE_INCLUDE_FAUST 0
 #endif
 
 #ifndef    HISE_FAUST_USE_LLVM_JIT
